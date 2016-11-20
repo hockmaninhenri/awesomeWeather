454f15ad57fb41ecbad04f37c03a8c2c1c0bfbbb
Henri Hockman <hockmaninhenri@gmail.com>
2016-11-20 13:14:20 +0200

    Added constants.ts, iconfont

--- platforms/ios/build/emulator/awesomeWeather.app/app/pages/main/main.component.js
+++ platforms/ios/build/emulator/awesomeWeather.app/app/pages/main/main.component.js
@@ -2,13 +2,25 @@
 var page_1 = require("ui/page");
 var router_1 = require("@angular/router");
 var core_1 = require("@angular/core");
+var textViewModule = require("ui/text-view");
+var observable = require("data/observable");
 var MainComponent = (function () {
     function MainComponent(router, page) {
         this.router = router;
         this.page = page;
     }
     MainComponent.prototype.ngOnInit = function () {
-        //this.page.actionBarHidden = true;
+        this.page.actionBarHidden = true;
+        function pageLoaded(args) {
+            var page = args.object;
+            var obj = new observable.Observable();
+            obj.set("someProperty", "Please change this text!");
+            page.bindingContext = obj;
+        }
+        exports.pageLoaded = pageLoaded;
+        var textView = new textViewModule.TextView();
+        textView.text = "I'ts damn cold outside";
+        textView.editable = false;
     };
     MainComponent = __decorate([
         core_1.Component({
--- platforms/ios/build/emulator/awesomeWeather.app/app/pages/main/main.component.ts
+++ platforms/ios/build/emulator/awesomeWeather.app/app/pages/main/main.component.ts
@@ -4,6 +4,9 @@ import { Page } from "ui/page";
 import { Router } from "@angular/router";
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
+import textViewModule = require("ui/text-view");
+import observable = require("data/observable");
+
 @Component({
   selector: "my-app",
   templateUrl: "pages/main/main.html",
@@ -14,6 +17,18 @@ export class MainComponent implements OnInit {
 
   }
   ngOnInit() {
-    //this.page.actionBarHidden = true;
+    this.page.actionBarHidden = true;
+
+    function pageLoaded(args) {
+      var page = args.object;
+      var obj = new observable.Observable();
+      obj.set("someProperty", "Please change this text!");
+      page.bindingContext = obj;
+    }
+    exports.pageLoaded = pageLoaded;
+
+    var textView = new textViewModule.TextView();
+    textView.text = "I'ts damn cold outside";
+    textView.editable = false;
   }
 }
